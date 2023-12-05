6e218835ca4081bf6c17d38fb8b56b893d10ec5b
Laura Lepannetier <74555001+laura-lpn@users.noreply.github.com>
2023-12-05 10:28:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'https://localhost:8000/api',
+        target: 'https://admin.asyafood.fr/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false, // Ignorer les erreurs de certificat
