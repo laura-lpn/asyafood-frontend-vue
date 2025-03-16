17145c4af538562397b5813a92cf802c3baf1efb
laura-lpn <74555001+laura-lpn@users.noreply.github.com>
2025-03-16 12:26:09 +0100

    fix: clean up MyHeader component template and improve mobile menu functionality

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,26 +1,24 @@
 <template>
   <header
-  class="
-  bg-white lg:bg-white-transparent shadow-main
-  py-3 lg:px-20
-  w-full z-[100] h-auto fixed
-  flex justify-center items-center flex-col
-  ">
-    <div
-    class="relative flex justify-center items-center w-full
+    class="
+    bg-white lg:bg-white-transparent shadow-main
+    py-3 lg:px-20
+    w-full z-[100] h-auto fixed
+    flex justify-center items-center flex-col
     ">
+    <div class="relative flex justify-center items-center w-full">
       <RouterLink :to="{ name: 'home'}" title="Accueil" class="hover:animate-pulse z-[100]">
         <img src="/logo.svg" alt="logo Asya Food" class="h-16 lg:h-20" />
       </RouterLink>
-      <nav 
-      class="absolute left-7 text-xl flex gap-5 lg:right-0 lg:left-auto
-      ">
+      <nav class="absolute left-7 text-xl flex gap-5 lg:right-0 lg:left-auto">
         <RouterLink :to="{name : 'list'}" title="Liste de course"
-        class="text-yellow lg:text-red hover:text-yellow
-        "><font-awesome-icon :icon="['fas', 'list-check']" /></RouterLink>
+          class="text-yellow lg:text-red hover:text-yellow">
+          <font-awesome-icon :icon="['fas', 'list-check']" />
+        </RouterLink>
         <RouterLink :to="{name : 'account'}" title="Mon compte"
-        class="text-yellow lg:text-red hover:text-yellow
-        "><font-awesome-icon :icon="['far', 'user']" /></RouterLink>
+          class="text-yellow lg:text-red hover:text-yellow">
+          <font-awesome-icon :icon="['far', 'user']" />
+        </RouterLink>
       </nav>
       <nav class="flex items-end flex-col font-second text-base bg-white absolute w-auto rounded-l-main pb-5 pl-10 text-right top-2 right-0 pr-7 transition lg:hidden">
         <button class="cursor-pointer text-3xl text-red" @click="toggleMobileMenu">
@@ -29,32 +27,28 @@
         </button>
         <ul id="mobile-menu" :class="{ 'hidden max-h-0': !mobileMenuOpen, 'flex flex-col gap-4 pb-2 mt-2 max-h-max overflow-hidden transition-all duration-300' : mobileMenuOpen}">
           <li v-for="category in categoriesWithRecipe" class="text-black font-medium" :key="category.id">
-            <Navlink :text="category.name" :slug="category.slug"/>
+            <Navlink :text="category.name" :slug="category.slug" @click="selectCategory" />
           </li>
         </ul>
-    </nav>
+      </nav>
     </div>
-    <nav
-    class=" 
-    gap-6 items-center justify-between w-full text-black font-second font-medium text-base hidden lg:flex">
+    <nav class="gap-6 items-center justify-between w-full text-black font-second font-medium text-base hidden lg:flex">
       <Navlink v-for="category in categoriesWithRecipe" :text="category.name" :slug="category.slug" :key="category.id"/>
     </nav>
   </header>
-
 </template>
 
 <script>
+import Navlink from './button/NavLink.vue';
 
-  import Navlink from './button/NavLink.vue';
-  
 export default {
   components: {
     Navlink,
   },
   data() {
     return {
-        categories: [],
-        mobileMenuOpen: false
+      categories: [],
+      mobileMenuOpen: false
     }
   },
   computed: {
@@ -70,6 +64,9 @@ export default {
     },
     toggleMobileMenu() {
       this.mobileMenuOpen = !this.mobileMenuOpen;
+    },
+    selectCategory() {
+      this.mobileMenuOpen = false;
     }
   },
   async created() {
