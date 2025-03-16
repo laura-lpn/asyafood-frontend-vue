abfcd28d6792e356d1d20fdd12a5141e8771dd52
laura-lpn <74555001+laura-lpn@users.noreply.github.com>
2025-03-16 11:52:50 +0100

    fix: update layout in MotifTitleRecipe component for better responsiveness

--- src/components/section/MotifTitleRecipe.vue
+++ src/components/section/MotifTitleRecipe.vue
@@ -24,7 +24,7 @@ export default {
 
 <template>
     <div
-    class="px-[5%] mt-10 flex items-center justify-center gap-3 md:gap-4 md:px-[10%]
+    class="px-[5%] mt-10 flex items-center justify-center flex-col lg:flex-row gap-0 lg:gap-3 md:gap-4 md:px-[10%]
     ">
         <img :src="motif" :alt="alt" class="w-16 h-16 bg-cover md:w-24 md:h-24 lg:w-32 lg:h-32">
         <h1 class="font-second text-2xl leading-xl font-bold md:text-3xl md:leading-4xl lg:text-5xl lg:leading-6xl" :style="[`color: ${color}`]">{{text}}</h1>
