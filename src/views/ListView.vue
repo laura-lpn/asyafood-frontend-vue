b6cba713ca8ee29d045ba73f48f8aa221cbcd6ae
laura-lpn <lauralepannetier@gmail.com>
2025-03-17 17:12:21 +0100

    fix: update ListView to display pluralized ingredient names based on quantity

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -13,7 +13,11 @@
             </span>
         </div>
         <div v-for="item in list" class="flex items-center justify-between w-full mb-4 px-4" :key="item.id">
-            <p :class="item.validate ? 'line-through' : null" class="lg:text-base text-left">{{ item.quantity ? item.quantity : null }} {{ item.unit ? item.unit : null }} {{ item.ingredient.name }}</p>
+            <p :class="item.validate ? 'line-through' : null" class="lg:text-base text-left">
+                {{ item.quantity ? item.quantity : null }} 
+                {{ item.unit ? item.unit : null }} 
+                {{ item.quantity > 1 ? item.ingredient.namePlurial : item.ingredient.name }}
+            </p>
             <input type="checkbox" name="validate" @change="toggleValidation(item)" id="validate" :checked="item.validate" class="w-5 h-5 accent-yellow cursor-pointer">
         </div>
         <div v-if="list.length == 0" class="text-black my-4">
