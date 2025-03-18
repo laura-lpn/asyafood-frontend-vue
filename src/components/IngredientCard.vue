6250e2e35fd99d76d1b8b6503fc38d04107d3c9b
laura-lpn <lauralepannetier@gmail.com>
2025-03-18 02:10:06 +0100

    refactor: update IngredientCard to display cleaned ingredient names and adjust height for better visibility

--- src/components/IngredientCard.vue
+++ src/components/IngredientCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="rounded-input overflow-hidden shadow-main h-10 relative">
+  <div class="rounded-input overflow-hidden shadow-main h-56 relative">
     <img :src="image" :alt="altImage" class="w-full h-full bg-center bg-cover z-0 object-cover" />
     <div
       class="absolute h-1/3 md:h-1/4 leading-5 bg-transparent-title w-full z-10 bottom-0 py-1 md:py-2 px-2 md:px-3 lg:px-4">
       <h3 class="text-white flex items-center text-sm leading-5 lg:leading-7 text-medium w-full h-full overflow-y-clip md:text-base lg:text-xl">
-        {{ name }}
+        {{ cleanName }}
       </h3>
     </div>
   </div>
@@ -26,6 +26,11 @@ export default {
     name: {
       type: String,
       required: true
+    }
+  },
+  computed: {
+    cleanName() {
+      return this.name.replace(/^(d'|de\s)/i, '');
     }
   }
 }
